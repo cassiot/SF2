9b5be5dbe5dda959e432661b190a605400e9335f
Cassio Tavares <cassiot@gmail.com>
2024-06-21 01:11:12 -0300

    create StreeFighterGame class to handle game init

---
+++ src/StreetFighterGame.js
@@ -0,0 +1,113 @@
+import { Ken } from "./entities/fighters/Ken.js";
+import { Ryu } from "./entities/fighters/Ryu.js";
+import { Stage } from "./entities/Stage.js";
+import { FpsCounter } from "./FpsCounter.js";
+import { STAGE_FLOOR } from "./constants/stage.js";
+import { FighterDirection, FighterState } from "./constants/fighters.js";
+
+export class StreetFighterGame {
+
+    constructor() {
+        this.debugImageIndex = 1;
+        this.allCanvas = document.querySelectorAll('canvas');
+
+        this.context = this.getContext();
+        this.debugContext = this.getDebugContext();
+
+        this.fighters = [
+            new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
+            new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
+        ]
+
+        this.entities = [
+            new Stage(),
+            ...this.fighters,
+            new FpsCounter()
+        ];
+
+        this.frameTime = {
+            previous: 0,
+            secondsPassed: 0
+        }
+    }
+
+    getContext() {
+        const canvasEl = this.allCanvas[0];
+        const context = canvasEl.getContext('2d');
+        context.imageSmoothingEnabled = false;
+        return context;
+    }
+
+    getDebugContext() {
+        const debugCanvas = this.allCanvas[1];
+        const debugContext = debugCanvas.getContext('2d');
+        return debugContext;
+    }
+
+    update() {
+        for (const entity of this.entities) {
+            entity.update(this.frameTime, this.context);
+        }
+    }
+
+    draw() {
+        for (const entity of this.entities) {
+            entity.draw(this.context);
+        }
+    }
+
+    frame(time) {
+        window.requestAnimationFrame(this.frame.bind(this));
+
+        this.frameTime = {
+            secondsPassed: (time - this.frameTime.previous) / 1000,
+            previous: time
+        }
+
+        this.update();
+        this.draw();
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+
+        const selectedCheckboxes = Array.from(event.target.querySelectorAll('input:checked')).map(checkbox => checkbox.value);
+        const options = event.target.querySelector('select');
+
+        this.fighters.forEach(fighter => {
+            if (selectedCheckboxes.includes(fighter.name)) {
+                fighter.changeState(options.value);
+            }
+        });
+    }
+
+    handleDebugCanvas(event) {
+        onkeypress = (event) => {
+            if (event.code == 'Space') {
+                // console.log('test');
+                const fighter0 = this.fighters[0];
+                const fighter1 = this.fighters[1];
+
+                var len = fighter1.animations[fighter1.currentState].length;
+
+                const [frameKey] = fighter1.animations[fighter1.currentState][fighter1.animationFrame];
+
+                const [[x, y, width, height],
+                    [originX, originY]] = fighter1.frames.get(`jumpUp-${this.debugImageIndex}`);
+
+                this.debugContext.reset();
+                this.debugContext.drawImage(fighter1.image, x, y, width, height, 5, 5, width, height);
+                this.debugImageIndex++;
+                if (this.debugImageIndex > len)
+                    this.debugImageIndex = 1;
+            }
+        };
+    }
+
+    start() {
+        document.addEventListener('submit', this.handleFormSubmit.bind(this));
+        document.addEventListener('keypress', this.handleDebugCanvas.bind(this));
+
+        window.requestAnimationFrame(this.frame.bind(this));
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { Ken } from "./entities/fighters/Ken.js";
-import { Ryu } from "./entities/fighters/Ryu.js";
-import { Stage } from "./entities/Stage.js";
-import { FpsCounter } from "./FpsCounter.js";
-import { STAGE_FLOOR } from "./constants/stage.js";
-import { FighterDirection, FighterState } from "./constants/fighters.js";
+import { StreetFighterGame } from "./StreetFighterGame.js";
+import { FighterState } from "./constants/fighters.js";
 
 function populateMoveDropdown() {
     const dropdown = document.getElementById('state-dropdown');
@@ -16,83 +12,8 @@ function populateMoveDropdown() {
     });
 }
 
-function handleFormSubmit(event, fighters) {
-    event.preventDefault();
-
-    const selectedCheckboxes = Array.from(event.target.querySelectorAll('input:checked')).map(checkbox => checkbox.value);
-    const options = event.target.querySelector('select');
-
-    fighters.forEach(fighter => {
-        if (selectedCheckboxes.includes(fighter.name)) {
-            fighter.changeState(options.value);
-        }
-    });
-}
-
 window.addEventListener('load', function () {
     populateMoveDropdown();
 
-    var canvasAll = document.querySelectorAll('canvas'); 
-    const canvasEl = canvasAll[0];
-    const canvasTest = canvasAll[1];
-
-    const context = canvasEl.getContext('2d');
-    const contextTest = canvasTest.getContext('2d');
-
-    const fighters = [
-        new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
-        new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
-    ]
-
-    context.imageSmoothingEnabled = false;
-
-    const entities = [
-        new Stage(),
-        ...fighters,
-        new FpsCounter()
-    ];
-
-    let frameTime = {
-        previous: 0,
-        secondsPassed: 0
-    }
-
-    function frame(time) {
-        window.requestAnimationFrame(frame);
-
-        frameTime = {
-            secondsPassed: (time - frameTime.previous) / 1000,
-            previous: time
-        }
-
-        for (const entity of entities) {
-            entity.update(frameTime, context);
-        }
-
-        for (const entity of entities) {
-            entity.draw(context);
-        }
-    }
-
-    var imgIndex = 1;
-    this.document.addEventListener('submit', (event) => handleFormSubmit(event, fighters));
-    onkeypress = (event) => {
-        if(event.code == 'Space'){
-            console.log('test');
-            var ken = fighters[1];
-            
-            const [frameKey] = ken.animations[ken.currentState][ken.animationFrame];
-        
-            const [[x, y, width, height],
-                [originX, originY]] = ken.frames.get(`jumpUp-${imgIndex}`);
-    
-                contextTest.reset();
-            contextTest.drawImage(ken.image, x, y, width, height, 5, 5, width, height);
-            imgIndex++;
-            if(imgIndex > 7)
-            imgIndex = 1;
-        }
-    };
-
-    window.requestAnimationFrame(frame);
+    new StreetFighterGame().start();
 });
